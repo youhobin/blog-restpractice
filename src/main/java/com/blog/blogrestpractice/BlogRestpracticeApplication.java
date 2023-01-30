8040d95d13e8bd14ae43f46f83c9be250f7026ee
youhobin <ghdb132@naver.com>
2023-01-30 13:02:22 +0900

    Initial commit

---
+++ src/main/java/com/blog/blogrestpractice/BlogRestpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogrestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRestpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogRestpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/blogrestpractice/BlogRestpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blogrestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
