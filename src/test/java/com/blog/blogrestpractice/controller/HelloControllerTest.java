7dfc280f3dfc1c0668c6f8da3d1a8096e1cdb793
youhobin <ghdb132@naver.com>
2023-01-30 13:18:58 +0900

    feat : HelloController 구현 및 테스트 코드 작성

---
+++ src/test/java/com/blog/blogrestpractice/controller/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.blog.blogrestpractice.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void helloTest() throws Exception {
+        //given
+        String hello = "hello!!";
+
+        //when
+
+        //then
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+            .andExpect(status().isOk())
+            .andExpect(content().string(hello));
+    }
+}
