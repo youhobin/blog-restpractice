559ff1f81cf54f07db3eb2a7a43d0d3e851853c2
youhobin <ghdb132@naver.com>
2023-01-30 13:33:58 +0900

    feat : User 엔티티 생성

---
+++ src/main/java/com/blog/blogrestpractice/domain/user/User.java
@@ -0,0 +1,38 @@
+package com.blog.blogrestpractice.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class User {
+
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Role role;
+
+}
